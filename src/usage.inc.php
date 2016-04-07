9ca41fedf0e70f6ecb63bb920212b0858f6fec82
William Petit <wpetit@cadoles.com>
2016-04-07 10:23:59 +0200

    Override templates dir via phpdocmd arguments

--- src/usage.inc.php
+++ src/usage.inc.php
@@ -14,3 +14,6 @@ Options:
     --lt [template]
         This specifies the 'template' for links we're generating. By default
         this is "%c.md".
+    --templates [templatesDir]
+        This specifies the path to the templates directory. By default this
+        is phpdoc-md/templates
